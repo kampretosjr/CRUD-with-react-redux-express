57cdd3acaf0453173d170ba9c1eefec199415763
khadam ikhwanus shofa <33866110+kampretosjr@users.noreply.github.com>
2019-07-19 00:44:56 +0700

    Update events_index.js

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,82 +1,96 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import _ from 'lodash'
-import { Link } from 'react-router-dom'
-import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn
-} from 'material-ui/Table'
-import FloatingActionButton from 'material-ui/FloatingActionButton'
-import ContentAdd from 'material-ui/svg-icons/content/add'
-
+import MaterialTable from 'material-table'
 import { readEvents } from '../actions'
 
+function text(text) {
+  if (text.length > 150) {
+      let textSplit = text.substr(0, 150)
+      return `${textSplit} ...`
+  } else {
+      let textSplit = text
+      return `${textSplit}`
+  }
+}
+
 class EventsIndex extends Component {
-  componentDidMount() {
-    this.props.readEvents()
-  }
+//bikin sate kosongan 
+  state = {
+    listData: [],
+  };
 
-  renderEvents() {
-    return _.map(this.props.events, (event,index) => (
-      <TableRow key={index}>
-      
-        <TableRowColumn>{event.id}</TableRowColumn>
-        <TableRowColumn>
-          <Link to={`/events/${event.id}`}>
-            {event.nama_buku}
-          </Link>
-        </TableRowColumn>
-        <TableRowColumn>{event.lokasi}</TableRowColumn>
-        <TableRowColumn><img src={event.foto_sampul} style={{width:"100px", height:"100px"}}></img></TableRowColumn>
-        <TableRowColumn>{event.deskripsi}</TableRowColumn>
-        
-      </TableRow>
-    ))
-  }
-
+//mengolah data yang sudah di tarik dari action 
+  componentDidMount = async () => {
+    await this.props.dispatch(readEvents());
+    this.setState({
+      //tampung di state list data
+      listData: this.props.events,
+    });
+  };
+  
   render() {
-    const style = {
-      position: "fixed",
-      right: 12,
-      bottom: 12
-    }
+    console.log("evvent",this.props.events)
+    console.log("props",this.props)
+    console.log("ini list",list)
+    
+    const list = this.state.listData;       //tampung state di variable baru
+    const arrayBaru = (Object.values(list)) // ubah kejadi array
     return (
-      <React.Fragment>
-        <FloatingActionButton style={style} containerElement={<Link to="/events/new" />}>
-          <ContentAdd />
-        </FloatingActionButton>
-        <Table>
-          <TableHeader
-            displaySelectAll={false}
-            adjustForCheckbox={false}
-          >
-            <TableRow>
-              <TableHeaderColumn>ID</TableHeaderColumn>
-              <TableHeaderColumn>Title</TableHeaderColumn>
-              <TableHeaderColumn>lokasi</TableHeaderColumn>
-              <TableHeaderColumn>foto</TableHeaderColumn>
-              <TableHeaderColumn>Body</TableHeaderColumn>
-            </TableRow>
-          </TableHeader>
-
-          <TableBody
-            displayRowCheckbox={false}
-          >
-            {this.renderEvents()}
-          </TableBody>
-        </Table>
-
-      </React.Fragment>
+      <div className="container">
+        <div className="mt-5">
+        <MaterialTable
+          title="Data Peminjaman Buku"
+          columns={[
+            {
+              title: 'Avatar',
+              field: 'e',
+              render: rowData => (
+                <img src={rowData.e} style={{width:"100px", height:"100px"}}></img>
+              ),
+            },
+            { title: 'No',       field: 'f' },
+            { title: 'judul',    field: 'a' },
+            { title: 'writer',   field: 'b' },
+            { title: 'location', field: 'c' },
+            { title: 'desc',     field: 'd' },
+            
+          ]}
+          data= {arrayBaru.map((ress, index) =>{
+            return(
+              {
+                f: ress.id,
+                a: ress.nama_buku,
+                b: ress.pengarang,
+                c: ress.lokasi,
+                d: text(ress.deskripsi),
+                e:ress.foto_sampul,
+              }
+            ) 
+                }
+              )
+            
+          }       
+          //  data={[
+          //    list]}
+              
+          actions={[
+            {
+              icon: 'add',
+              tooltip: 'Add User',
+              isFreeAction: true,
+              
+              //onClick: (event) => alert("You want to add a new row")
+            }
+          ]}
+        />
+      
+      </div></div>
     )
   }
 }
 
 const mapStateToProps = state => ({ events: state.events })
 
-const mapDispatchToProps = ({ readEvents })
+//const mapDispatchToProps = ({ readEvents })
 
-export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
+export default connect(mapStateToProps)(EventsIndex)
